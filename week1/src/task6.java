79e9b4393cf2001a4aa1477d9c8149aa97565235
c3-ck-task1 <2101431174@qq.com>
2021-01-29 19:38:06 +0800

    第六个任务不停冲！

--- week1/src/task6.java
+++ week1/src/task6.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
 
 public class task6 {
     public static void main(String[] args) {
-        BankAcount BA = new BankAcount();
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("请选择服务类型");
+        System.out.println("1       开户");
+        System.out.println("2    查询余额");
+        String name;
+        double money;
+        int x;
+        x = in.nextInt();
+        if(x == 1)
+        {
+            System.out.println("请输入您的姓名:");
+            name = in.nextLine();
+            System.out.println("请输入您的余额:");
+            money = in.nextDouble();
+            BankAcount BA = new BankAcount(name,money);
+        }
+        else
+        {
+            String Name;
+            double balance;
+            System.out.println("请输入您的姓名");
+            Name = in.nextLine();
+            BankAcount BA = new BankAcount(Name);
+        }
 
     }
 }
@@ -13,5 +38,8 @@ class BankAcount{
     public BankAcount(String Username, double balance){
         this.Username=Username;
         this.balance=balance;
+    }
+    public BankAcount(String Username){
+        System.out.println("您的账户余额为：" + balance);
     }
 }
