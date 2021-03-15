5ac467a03c226f7d9b652bb1408953b7515dcd39
c3-ck-task1 <2101431174@qq.com>
2021-03-15 16:29:54 +0800

    第五周1个任务。。。

---
+++ week5/src/task1.sql
@@ -0,0 +1,9 @@
+CREATE TABLE cqupt_student(
+                              studentid VARCHAR(10),
+                              NAME VARCHAR(20),
+                              sex VARCHAR(2),
+                              age INTEGER,
+                              Fee DECIMAL(10,2),
+                              address VARCHAR(50),
+                              memo VARCHAR(300)
+);
